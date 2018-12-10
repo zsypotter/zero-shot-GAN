c3c1db94596a55245480ee5e711bc8378214d5f2
zsypotter <zsypotter@gmail.com>
2018-12-10 13:54:40 +0800

    add args

--- main.py
+++ main.py
@@ -25,34 +25,35 @@ print("Random Seed: ", manualSeed)
 random.seed(manualSeed)
 torch.manual_seed(manualSeed)
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataroot", type=str, default="/data2/zhousiyu/dataset/Animals_with_Attributes2/JPEGImages")
+parser.add_argument("--workers", type=int, default=2)
+parser.add_argument("--ngpu", type=int, default=1)
+parser.add_argument("--batch_size", type=int, default=128)
+parser.add_argument("--image_size", type=int, default=64)
+parser.add_argument("--nc", type=int, default=3)
+parser.add_argument("--nz", type=int, default=100)
+parser.add_argument("--ndf", type=int, default=64)
+parser.add_argument("--ngf", type=int, default=64)
+parser.add_argument("--num_epochs", type=int, default=100)
+parser.add_argument("--lr", type=float, default=0.0002)
+parser.add_argument("--beta1", type=float, default=0.5)
+parser.add_argument("--gp_weight", type=float, default=10.)
+args = parser.parse_args()
 
-dataroot = "/data2/zhousiyu/dataset/Animals_with_Attributes2/JPEGImages"
-workers = 2
-batch_size = 128
-image_size = 64
-nc = 3
-nz = 100
-ngf = 64
-ndf = 64
-num_epochs = 100
-lr = 0.0002
-beta1 = 0.5
-ngpu = 1
-gp_weight = 10
-
-dataset = dset.ImageFolder(root=dataroot,
+dataset = dset.ImageFolder(root=args.dataroot,
                            transform=transforms.Compose([
-                               transforms.Resize(image_size),
-                               transforms.CenterCrop(image_size),
+                               transforms.Resize(args.image_size),
+                               transforms.CenterCrop(args.image_size),
                                transforms.ToTensor(),
                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                            ]))
 # Create the dataloader
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-                                         shuffle=True, num_workers=workers)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size,
+                                         shuffle=True, num_workers=args.workers)
 
 # Decide which device we want to run on
-device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+device = torch.device("cuda:0" if (torch.cuda.is_available() and args.ngpu > 0) else "cpu")
 
 
 def weights_init(m):
@@ -64,17 +65,17 @@ def weights_init(m):
         nn.init.constant_(m.bias.data, 0)
 
 # Create the generator
-netG = Generator(ngpu, nc, nz, ndf, ngf).to(device)
-if (device.type == 'cuda') and (ngpu > 1):
-    netG = nn.DataParallel(netG, list(range(ngpu)))
+netG = Generator(args.ngpu, args.nc, args.nz, args.ndf, args.ngf).to(device)
+if (device.type == 'cuda') and (args.ngpu > 1):
+    netG = nn.DataParallel(netG, list(range(args.ngpu)))
 netG.apply(weights_init)
 print(netG)
 
 
 # Create the Discriminator
-netD = Discriminator(ngpu, nc, nz, ndf, ngf).to(device)
-if (device.type == 'cuda') and (ngpu > 1):
-    netD = nn.DataParallel(netD, list(range(ngpu)))
+netD = Discriminator(args.ngpu, args.nc, args.nz, args.ndf, args.ngf).to(device)
+if (device.type == 'cuda') and (args.ngpu > 1):
+    netD = nn.DataParallel(netD, list(range(args.ngpu)))
 netD.apply(weights_init)
 print(netD)
 
@@ -84,15 +85,15 @@ criterion = nn.BCELoss()
 
 # Create batch of latent vectors that we will use to visualize
 #  the progression of the generator
-fixed_noise = torch.randn(64, nz, 1, 1, device=device)
+fixed_noise = torch.randn(64, args.nz, 1, 1, device=device)
 
 # Establish convention for real and fake labels during training
 real_label = 1
 fake_label = 0
 
 # Setup Adam optimizers for both G and D
-optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
-optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
+optimizerD = optim.Adam(netD.parameters(), lr=args.lr, betas=(args.beta1, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=args.lr, betas=(args.beta1, 0.999))
 
 
 # Training Loop
@@ -106,20 +107,20 @@ iters = 0
 writer = SummaryWriter()
 print("Starting Training Loop...")
 # For each epoch
-for epoch in range(num_epochs):
+for epoch in range(args.num_epochs):
     # For each batch in the dataloader
     for i, data in enumerate(dataloader, 0):
         
         ############################
-        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+        # (1) Update D network
         ###########################
         netD.zero_grad()
 
         real = data[0].to(device)
         b_size = real.size(0)
-        read_d = netD(real)
+        real_d = netD(real)
 
-        noise = torch.randn(b_size, nz, 1, 1, device=device)
+        noise = torch.randn(b_size, args.nz, 1, 1, device=device)
         fake = netG(noise).detach()
         fake_d = netD(fake)
 
@@ -132,18 +133,18 @@ for epoch in range(num_epochs):
             create_graph=True)[0]
         gp = ((gradients.view(b_size, -1).norm(2, dim=1) - 1).pow(2)).mean()
 
-        loss_d_without_gp = (-read_d + fake_d).mean()
-        loss_d = loss_d_without_gp + gp_weight * gp
+        loss_d_without_gp = (-real_d + fake_d).mean()
+        loss_d = loss_d_without_gp + args.gp_weight * gp
         loss_d.backward()
         optimizerD.step()
 
 
         ############################
-        # (2) Update G network: maximize log(D(G(z)))
+        # (2) Update G network
         ###########################
         netG.zero_grad()
 
-        noise = torch.randn(b_size, nz, 1, 1, device=device)
+        noise = torch.randn(b_size, args.nz, 1, 1, device=device)
         fake = netG(noise)
         fake_d = netD(fake).mean()
 
@@ -155,7 +156,7 @@ for epoch in range(num_epochs):
         # Output training stats
         if i % 1 == 0:
             print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tLoss_D_Without_GP: %.4f'
-                  % (epoch, num_epochs, i, len(dataloader),
+                  % (epoch, args.num_epochs, i, len(dataloader),
                      loss_d.item(), loss_g.item(), loss_d_without_gp.item()))
         
         
